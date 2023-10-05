8442fe7beb9e488941f15e4987a86c64659bd31b
JohnNixon6972 <johnbrightnixon@gmail.com>
2023-10-05 18:28:55 +0100

    added color codes

---
+++ lib/constants/appcolors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backGroundColor =  Color.fromARGB(255, 39, 54, 77);
+  static const textColor1 = Color.fromARGB(255, 255, 255, 255);
+  static const highlightColor = Color.fromARGB(255, 146, 163, 191);
+  static const textColor2 = Color.fromARGB(255, 175, 198, 236);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:in_sleep/constants/appcolors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.backGroundColor),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -81,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: AppColors.backGroundColor,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
